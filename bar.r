add08bfc727dce3c992649765d46d56c4cbcf23c
Meghan O'Neill <meghan.r.oneill@gmail.com>
2014-12-10 13:49:15 -0800

    Save to pdf and add subtitle

--- bar.r
+++ bar.r
@@ -1,6 +1,11 @@
+pdf(file = "Hello_world.pdf")
+
 bar  <- "Hello World!\nThis is a computer program."
 plot.new()
 text(1/2, 1/2, bar)
 
 norf <- "I Edited It!"
-title(norf)
+baz <- "And Saved The Plot To PDF"
+title(norf, baz)
+
+dev.off()
